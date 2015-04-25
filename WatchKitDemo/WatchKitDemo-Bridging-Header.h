176c8f384169e565898684906d95913608ebe887
Grady Zhuo <gradyzhuo@gmail.com>
2015-04-25 11:11:01 +0800

    1. Add Wormhole

---
+++ WatchKitDemo/WatchKitDemo-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  WatchKitDemo-Bridging-Header.h
+//  WatchKitDemo
+//
+//  Created by Grady Zhuo on 4/25/15.
+//  Copyright (c) 2015 Grady Zhuo. All rights reserved.
+//
+
+#import <MMWormhole/MMWormhole.h>
